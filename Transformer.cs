ba34f8e0bf41166f7e7a6cbfc3caa50534c6649e
Kalle Launiala <kalle.launiala@protonit.net>
2012-04-12 16:05:51 +0300

    Added basic structure

---
+++ Transformer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using CM=ClassModel_v1_0;
+using UI=UserInterface_v1_0;
+
+namespace UserInterfaceToClassModelTRANS
+{
+    public class Transformer
+    {
+        T LoadXml<T>(string xmlFileName)
+        {
+            using (FileStream fStream = File.OpenRead(xmlFileName))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                T result = (T)serializer.Deserialize(fStream);
+                fStream.Close();
+                return result;
+            }
+        }
+
+
+
+	    public Tuple<string, string>[] GetGeneratorContent(params string[] xmlFileNames)
+	    {
+            List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+            foreach(string xmlFileName in xmlFileNames)
+            {
+                UI.UserInterfaceAbstractionType fromAbs = LoadXml<UI.UserInterfaceAbstractionType>(xmlFileName);
+                CM.ClassModelAbstractionType  toAbs = TransformAbstraction(fromAbs);
+                string xmlContent = WriteToXmlString(toAbs);
+                FileInfo fInfo = new FileInfo(xmlFileName);
+                string contentFileName = "ClassModelFrom" + fInfo.Name;
+                result.Add(Tuple.Create(contentFileName, xmlContent));
+            }
+	        return result.ToArray();
+	    }
+
+        private string WriteToXmlString(CM.ClassModelAbstractionType toAbs)
+        {
+            XmlSerializer serializer = new XmlSerializer(toAbs.GetType());
+            MemoryStream memoryStream = new MemoryStream();
+            serializer.Serialize(memoryStream, toAbs);
+            byte[] data = memoryStream.ToArray();
+            string result = System.Text.Encoding.UTF8.GetString(data);
+            return result;
+        }
+
+        public static CM.ClassModelAbstractionType TransformAbstraction(UI.UserInterfaceAbstractionType fromAbs)
+        {
+            CM.ClassModelAbstractionType toAbs = null;
+            return toAbs;
+        }
+
+    }
+}
